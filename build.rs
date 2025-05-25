938276c71536013b618877b820af9bbfed1be1e8
Michael Laß <bevan@bi-co.net>
2025-05-25 10:44:59 +0200

    Fix clippy warning: using `contains()` instead of `iter().any()` is more efficient

--- build.rs
+++ build.rs
@@ -34,28 +34,28 @@ fn main() {
     println!("cargo::rustc-check-cfg=cfg(DAX)");
 
     // Introduced in v1.43
-    if CONSTANTS.iter().any(|&s| s == "EXT4_INLINE_DATA_FL") {
+    if CONSTANTS.contains(&"EXT4_INLINE_DATA_FL") {
         println!("cargo::rustc-cfg=INLINE_DATA");
     }
-    if CONSTANTS.iter().any(|&s| s == "EXT4_PROJINHERIT_FL") {
+    if CONSTANTS.contains(&"EXT4_PROJINHERIT_FL") {
         println!("cargo::rustc-cfg=PROJINHERIT");
     }
-    if CONSTANTS.iter().any(|&s| s == "EXT4_ENCRYPT_FL") {
+    if CONSTANTS.contains(&"EXT4_ENCRYPT_FL") {
         println!("cargo::rustc-cfg=ENCRYPT");
     }
 
     // Introduced in v1.44.4
-    if CONSTANTS.iter().any(|&s| s == "EXT4_VERITY_FL") {
+    if CONSTANTS.contains(&"EXT4_VERITY_FL") {
         println!("cargo::rustc-cfg=VERITY");
     }
 
     // Introduced in v1.45.0
-    if CONSTANTS.iter().any(|&s| s == "EXT4_CASEFOLD_FL") {
+    if CONSTANTS.contains(&"EXT4_CASEFOLD_FL") {
         println!("cargo::rustc-cfg=CASEFOLD");
     }
 
     // Introduced in v1.45.7
-    if CONSTANTS.iter().any(|&s| s == "FS_DAX_FL") {
+    if CONSTANTS.contains(&"FS_DAX_FL") {
         println!("cargo::rustc-cfg=DAX");
     }
 }
